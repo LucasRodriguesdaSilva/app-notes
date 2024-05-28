98d2b1e4dd6fbb65d970c67074560bf34bf21cb0
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-27 21:01:52 -0300

    feat: Criação dos repositories

---
+++ back/app/Models/Repositories/Anotacoes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Repositories;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Anotacoes extends Model
+{
+    use HasFactory;
+    
+}
