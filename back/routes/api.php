9b6e625b52fdf775eb3f40b065de0c96e283d4d7
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-27 21:03:25 -0300

    feat: Adicionado rotas do usuario e correção da rota web

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Usuario;
+use Illuminate\Http\Client\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/', function(){
+    return Response()->json(['Ok']);
 });
+
+Route::post('/user/cadastrar', [Usuario::class, 'cadastrar']);
